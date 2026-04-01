6e50e7121f5492ed7d122c85ff42f94599abf47b
minkel27 <156118425+minkel27@users.noreply.github.com>
2026-04-01 13:36:20 -0700

    Add files via upload

---
+++ Hazard_Detection/Testfile.cpp
@@ -0,0 +1,16 @@
+/*
+For:   Poly Rover Club
+From:  Evan
+Task:  Serve as a demo for camera video detection
+*/
+
+#include <stdio.h>
+
+int main(){
+    int a, b, c, sum;
+    a = b = c = 1;
+    sum = a+b+c;
+    printf("%d", sum);
+
+    return 0;
+};
